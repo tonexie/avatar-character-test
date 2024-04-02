9448b195caa58e03c5128d4982480ebb6810e980
tonexie <tone.xie@gmail.com>
2024-04-02 15:07:17 +1300

    initial setup, and basic structure

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let title = 'Avatar Trivia';
+</script>
+
+<header class="header  px-4 py-2 flex items-center justify-between">
+  <h1>{title}</h1>
+  </header>
+
+<style >
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import Hero from '$lib/components/Hero.svelte';
+</script>
+
+<div class="home-container">
+  <Header />
+  <Hero />
+</div>
+
+<style>
+  * {
+    border: black 1px solid;
+  }
+
+  .home-container {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
