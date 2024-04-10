7796ddb5d2c9527934ede37f44e64132674b60d1
tonexie <tone.xie@gmail.com>
2024-04-11 03:10:13 +1200

    add fade in for images

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,29 +1,34 @@
 <script>
-  const logo = '/images/avatar-logo.png'
+	const logo = '/images/avatar-logo.png';
 	import { preloadImages } from '$lib/utils/preloadImages.js';
-  preloadImages([`/images/avatar-hero-v2.jpg`]);
+	async () => {
+		await preloadImages(['/images/avatar-hero-v2.jpg']);
+	};
 </script>
 
-<header class='py-4 px-4 sm:px-10  font-[sans-serif] min-h-[70px]'>
-  <div class='flex flex-wrap items-center justify-between gap-5 relative'>
-    <a href="/"><img src={logo} alt="logo" class='w-36' />
-    </a>
-    <div class='flex lg:order-1 max-sm:ml-auto'>
-      <button
-        class='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#003049] bg-[#003049] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#003049]'>Info</button>
-      <button
-        class='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#003049] bg-[#003049] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#003049] ml-3'>Credits</button>
-    </div>
-  </div>
+<header class="py-4 px-4 sm:px-10 font-[sans-serif] min-h-[70px]">
+	<div class="flex flex-wrap items-center justify-between gap-5 relative">
+		<a href="/"><img src={logo} alt="logo" class="w-36" /> </a>
+		<div class="flex lg:order-1 max-sm:ml-auto">
+			<button
+				class="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#003049] bg-[#003049] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#003049]"
+				>Info</button
+			>
+			<button
+				class="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#003049] bg-[#003049] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#003049] ml-3"
+				>Credits</button
+			>
+		</div>
+	</div>
 </header>
 
 <style>
-  header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 100;
-    background-color: rgba(255, 255, 255, 0);
-  }
+	header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index: 100;
+		background-color: rgba(255, 255, 255, 0);
+	}
 </style>
--- src/routes/result/[slug]/+page.svelte
+++ src/routes/result/[slug]/+page.svelte
@@ -1,7 +1,16 @@
 <script>
 	import BackgroundImg from '$lib/components/BackgroundImg.svelte';
 	import { characters } from '$lib/data/results.js';
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
 	export let data;
+	let key = 0;
+
+	// allow for animation on first render
+	onMount(() => {
+		key++;
+	});
 
 	let character = characters.find((char) => data.title === char.slug);
 </script>
@@ -10,16 +19,21 @@
 
 <div class="flex justify-center items-center h-screen">
 	{#if character}
-		<div class="flex flex-col items-center justify-center max-w-md rounded-lg shadow-lg bg-white h-[90%]">
-			<div>
-				<img class="w-full rounded object-cover" src={character.imgUrl} alt={character.name} />
+		{#key key}
+			<div
+				class="flex flex-col items-center justify-center max-w-md rounded-lg shadow-lg bg-white h-[90%]"
+				in:fade={{ delay: 600, duration: 500 }}
+			>
+				<div>
+					<img class="w-full rounded object-cover" src={character.imgUrl} alt={character.name} />
+				</div>
+				<div class="px-6 py-4">
+					<h1 class="font-bold text-xl mb-2">{character.name}</h1>
+					{#each character.traits as trait}
+						<p class="text-gray-700 text-base">{trait}</p>
+					{/each}
+				</div>
 			</div>
-			<div class="px-6 py-4">
-				<h1 class="font-bold text-xl mb-2">{character.name}</h1>
-				{#each character.traits as trait}
-					<p class="text-gray-700 text-base">{trait}</p>
-				{/each}
-			</div>
-		</div>
+		{/key}
 	{/if}
 </div>
