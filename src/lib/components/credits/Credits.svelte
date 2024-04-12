db652ca3a101c886af7654f054681f9d883b93ae
tonexie <tone.xie@gmail.com>
2024-04-13 02:04:55 +1200

    credits using table

--- src/lib/components/credits/Credits.svelte
+++ src/lib/components/credits/Credits.svelte
@@ -1,12 +1,41 @@
 <script>
 	import creditsList from '$lib/components/credits/credits.js';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+    Banner 
+	} from 'flowbite-svelte';
 </script>
 
 <div class="flex justify-center items-center w-full h-full mt-8">
 	<div
-		class="flex flex-col justify-start items-center max-w-xl bg-sky-500 rounded-lg p-8 my-8 shadow-lg"
+		class="flex flex-col justify-start items-center max-w-xl  rounded-lg p-8 my-8 shadow-lg"
 	>
+  <Banner>
 		<h1 class="text-4xl font-bold text-white mb-4 underline">Credits</h1>
+    
+  </Banner>
+		<Table color="blue" hoverable={true}>
+			<TableHead>
+				<TableHeadCell>Image Title</TableHeadCell>
+				<TableHeadCell>Artist</TableHeadCell>
+				<TableHeadCell>Image URL</TableHeadCell>
+			</TableHead>
+			<TableBody>
+				{#each creditsList as source}
+					<TableBodyRow>
+						<TableBodyCell>{source.title}</TableBodyCell>
+						<TableBodyCell>{source.author}</TableBodyCell>
+						<TableBodyCell><a href={source.url}>Click Here</a></TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
+
 		<div class="flex flex-col w-full bg-blue-50 p-8 rounded">
 			{#each creditsList as source}
 				<li class="mb-2">
